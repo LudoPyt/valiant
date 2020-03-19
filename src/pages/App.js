d48a2db03b6996f5c96809b99d3f5a4b89e6c192
Pouyaut Ludovic <ludovic.pouyaut@gmail.com>
2020-03-19 15:36:43 +0100

    fix App.js

--- src/pages/App.js
+++ src/pages/App.js
@@ -1,18 +1,18 @@
 import React from 'react';
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Home from './Home';
 import Story from './Story';
 
 const App = () => (
   <Router>
-      <Switch>
-        <Route path="/">
-          <Home />
-        </Route>
-        <Route path="/story">
-          <Story />
-        </Route>
-      </Switch>
+    <Switch>
+      <Route path="/story">
+        <Story />
+      </Route>
+      <Route path="/">
+        <Home />
+      </Route>
+    </Switch>
   </Router>
 );
 
