22285b0ea0527ae5239c6997419c478525dc2e65
Morgane <morgane@gotoandbuzz.com>
2020-04-14 15:37:45 +0200

    Simon game

--- src/pages/App.js
+++ src/pages/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Home from './Home';
 import Story from './Story';
+import Alaska from '../components/Alaska';
 import Alaska1 from '../components/Alaska1';
 import Alaska2 from '../components/Alaska2';
 import Alaska3 from '../components/Alaska3';
@@ -12,6 +13,7 @@ import '../scss/basic.scss';
 const App = () => (
   <Router>
     <Switch>
+      <Route path="/story0" component={Alaska} />
       <Route path="/story1" component={Alaska1} />
       <Route path="/story2" component={Alaska2} />
       <Route path="/story3" component={Alaska3} />
