a5d74fd6ca46de427ced96c231535567bc1cb3d4
Pouyaut Ludovic <ludovic.pouyaut@gmail.com>
2020-04-20 18:09:05 +0200

    add config

--- src/components/CockpitScene.js
+++ src/components/CockpitScene.js
@@ -18,21 +18,19 @@ class CockpitScene{
         this.isStarted = false
         this.raf = 0;
         this.speedRot = THREE.Math.degToRad(45);
-        this.maxRotation = 0.5;
+        this.maxRotation = .5;
         this.soundRead = false;
         this.clock = new THREE.Clock();
         this.delta = 0;
         this.arrow = new ArrowMove();
         this.loader = new GLTFLoader();
-
+        this.axesHelper = new THREE.AxesHelper( 5 );
         //video
         this._setVideo();
         //son
         this._addSound();
-
-
-
-        this._setupEventListerner();
+        
+        this._setupEventListener();
         
     }
 
@@ -48,20 +46,20 @@ class CockpitScene{
           let gui = new dat.GUI();
       
           let cock = gui.addFolder('cockpit');
-          cock.add(object.position, 'y', -400, -100).listen();
+          cock.add(object.position, 'y', -100, 100).listen();
           cock.add(object.position, 'x', -100, 100).listen();
           cock.add(object.position, 'z', -100, 100).listen();
           cock.add(object.rotation, 'y', -2, 2).listen();
-          cock.add(object.rotation, 'x', -180, 180).listen();
-          cock.add(object.rotation, 'z', -180, 180).listen();
+          cock.add(object.rotation, 'x', -3, 3).listen();
+          cock.add(object.rotation, 'z', -3, 3).listen();
           cock.open();
       
           
           let cam = gui.addFolder('camera');
-          cam.add(this.camera.position, 'y', -100, 200).listen();
+          cam.add(this.camera.position, 'y', -100, 100).listen();
           cam.add(this.camera.position, 'x', -100, 100).listen();
-          cam.add(this.camera.position, 'z', -100, 200).listen();
-          cam.add(this.camera.rotation, 'y', -2, 2).listen();
+          cam.add(this.camera.position, 'z', -100, 100).listen();
+          cam.add(this.camera.rotation, 'y', -1, 1).listen();
           cam.add(this.camera.rotation, 'x', -10, 10).listen();
           cam.add(this.camera.rotation, 'z', -10, 10).listen();
           cam.open();
@@ -69,14 +67,14 @@ class CockpitScene{
           gui.add(options, 'reset');
     }
 
-    _setupEventListerner() {
+    _setupEventListener() {
         this.video.addEventListener('canplaythrough', () => {
             if (!this.isStarted){
                 this.isStarted = true
+
                 this._setScene();
                 this.controls = new OrbitControls( this.camera, this.renderer.domElement)
-                //manche
-                this._addStick();
+            
 
                 //floor
                 this._addFloor();
@@ -87,11 +85,9 @@ class CockpitScene{
                 this._setTextureVideo();
                 this._addWallLeft(this.textureVideo);
                 this._addWallRight(this.textureVideo);
-                this.scene.add(this.stick, this.floor);
-                this.scene.add(this.wallLeft, this.wallRight)
-                this._render();
+                this.scene.add( this.axesHelper );
 
-        
+                this._render();
             }
             })
     }
@@ -112,14 +108,15 @@ class CockpitScene{
             0.1,
             1000
         );
-        this.camera.lookAt(this.scene.position);
+        this.camera.position.set(0,0,0)
+        this.camera.lookAt(this.scene.position)
+
 
         let light = new THREE.AmbientLight(0xffffff, .5);
         this.scene.add(light);
 
         let lightPoint = new THREE.PointLight(0xffffff, 0.5);
         this.scene.add(lightPoint)
-        
     }
 
     _addSound(){
@@ -136,6 +133,9 @@ class CockpitScene{
     }
 
     _render() {
+        if (this.helices){
+            this.helices.rotation.x += 10 * this.delta
+        }
         if (
             (this.camera.rotation.y <= -0.3 && !this.soundRead) ||
             (this.camera.rotation.y >= 0.3 && !this.soundRead)
@@ -157,15 +157,18 @@ class CockpitScene{
         if (this.arrow.directions.backward) {
             this.camera.rotation.x += -this.speedRot * this.delta;
         }
-        if (this.arrow.directions.left && this.camera.rotation.y < this.maxRotation) {
+        if (this.arrow.directions.left && this.cockpit.rotation.z < this.maxRotation) {
             this.camera.rotateY(this.speedRot * this.delta);
-            // this.stick.rotateZ((this.speedRot * this.delta) / 2);
-            this.stick.rotateZ(this.speedRot * this.delta);
+            this.camera.rotateZ(this.speedRot * this.delta);
+            this.cockpit.rotateZ(this.speedRot * this.delta)
+            this.stick.rotateX(this.speedRot * this.delta);
         }
-        if (this.arrow.directions.right && this.camera.rotation.y > -this.maxRotation) {
+        if (this.arrow.directions.right && this.cockpit.rotation.z> -this.maxRotation) {
             this.camera.rotateY(-this.speedRot * this.delta);
-            // this.stick.rotateZ((-this.speedRot * this.delta) / 2);
-            this.stick.rotateZ(-this.speedRot * this.delta);
+            this.camera.rotateZ(-this.speedRot * this.delta);
+
+            this.cockpit.rotateZ(-this.speedRot * this.delta)
+            this.stick.rotateX(-this.speedRot * this.delta);
         }
 
         //avancé
@@ -174,7 +177,7 @@ class CockpitScene{
         
 
         if (!this.needDestroy) {
-            requestAnimationFrame(this._render.bind(this));
+           this.raf = requestAnimationFrame(this._render.bind(this));
         }
 
         this.renderer.render(this.scene, this.camera);
@@ -183,41 +186,48 @@ class CockpitScene{
 
     _addCockpit(){
         this.loader.load('Cockpit3D/scene.gltf', (object) => { 
-            
-            object.scene.scale.set(.04, .04, .04);
-            console.log(this)
-            object.scene.position.set(64, -300, -50);
-            this._setGUI(object.scene);
-          
-            this.scene.add(object.scene)
+            this.cockpit = object.scene
+            this.cockpit.scale.set(.04, .04, .04);
+            this.cockpit.position.set(0, -80, -85);
+
+            this._setGUI(this.cockpit);
+            this.scene.add(this.cockpit)
+            this.cockpit.traverse((child) => {
+                // console.log(child)
+                if (child.name === 'Helices' ){
+                    this.helices = child
+                }
+                if (child.name === 'Manche'){
+                    this._addStick(child)
+                }
+            })
         })
     }
     _addWallRight(texture) {
         let wallRightGeometry = new THREE.PlaneGeometry(50, 50);
         let wallRightMaterial = new THREE.MeshBasicMaterial({map: texture});
         this.wallRight = new THREE.Mesh(wallRightGeometry, wallRightMaterial);
-        this.wallRight.position.set(91, -12, -93);
+        this.wallRight.position.set(91, 0, -93);
         this.wallRight.rotation.set(0, -0.4, 0);
+        this.scene.add(this.wallRight)
+
     }
     _addWallLeft(texture) {
         let wallLeftGeometry = new THREE.PlaneGeometry(50, 50);
         let wallLeftMaterial = new THREE.MeshBasicMaterial({map: texture});
         this.wallLeft = new THREE.Mesh(wallLeftGeometry, wallLeftMaterial);
-        this.wallLeft.position.set(-91, -12, -93);
+        this.wallLeft.position.set(-91, 0, -93);
         this.wallLeft.rotation.set(0, 0.4, 0);
-    }
-
-    _addStick() {
-        let stickGeometry = new THREE.CylinderGeometry(5, 5, 50, 12);
-        stickGeometry.applyMatrix(new THREE.Matrix4().makeTranslation(0, 50 / 2, 0));
-        let stickMaterial = new THREE.MeshNormalMaterial();
-        this.stick = new THREE.Mesh(stickGeometry, stickMaterial);
-        this.camera.position.set(0, 100, 100);
-        this.camera.rotation.set(-0.3, 0, 0);
-        this.stick.add(this.camera);
-        this.stick.position.set(0, -75, -100);
+        this.scene.add(this.wallLeft)
 
     }
+
+    _addStick(child) {
+        this.stick = child
+
+
+    }
+
     _addFloor() {
         let floorGeometry = new THREE.PlaneGeometry(50, 1000, 5, 32);
         let floorMaterial = new THREE.MeshBasicMaterial({
@@ -226,7 +236,9 @@ class CockpitScene{
         });
         this.floor = new THREE.Mesh(floorGeometry, floorMaterial);
         this.floor.rotation.x += Math.PI / 2;
-        this.floor.position.set(0, -75, this.stick.position.z - 500);
+        this.floor.position.set(0, -100,0);
+        this.scene.add(this.floor);
+
     }
 
     _setVideo() {
