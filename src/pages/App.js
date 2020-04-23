0dd38d3133b6695e605f6eb3c4012192fb653888
Morgane <morgane@gotoandbuzz.com>
2020-04-23 17:46:12 +0200

    refacto files organistion

--- src/pages/App.js
+++ src/pages/App.js
@@ -2,11 +2,11 @@ import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Home from './Home';
 import Story from './Story';
-import AlaskaSimon from '../components/AlaskaSimon';
-import AlaskaTakeOff from '../components/AlaskaTakeOff';
-import AlaskaFlightFeelings from '../components/AlaskaFlightFeelings';
+import Chp1Simon from '../scenes/Chp1Simon';
+import Chp1TakeOff from '../scenes/Chp1TakeOff';
+import Chp2FlightFeelings from '../scenes/Chp2FlightFeelings';
 import Alaska3 from '../components/Alaska3';
-import AlaskaLanding from '../components/AlaskaLanding';
+import Chp3Landing from '../scenes/Chp3Landing';
 import NoMatch from './404';
 // import Nuages from '../components/Nuages';
 
@@ -15,11 +15,11 @@ import '../scss/basic.scss';
 const App = () => (
   <Router>
     <Switch>
-      <Route path="/simon" component={AlaskaSimon} />
-      <Route path="/takeoff" component={AlaskaTakeOff} />
-      <Route path="/flight-feelings" component={AlaskaFlightFeelings} />
+      <Route path="/simon" component={Chp1Simon} />
+      <Route path="/takeoff" component={Chp1TakeOff} />
+      <Route path="/flight-feelings" component={Chp2FlightFeelings} />
       <Route path="/story3" component={Alaska3} />
-      <Route path="/landing" component={AlaskaLanding} />
+      <Route path="/landing" component={Chp3Landing} />
       <Route path="/story" component={Story} />
       <Route path="/" component={Home} />
       <Route path="*" component={NoMatch} />
