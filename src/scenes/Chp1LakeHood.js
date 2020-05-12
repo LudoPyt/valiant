c78fea80c8379b8ff232910b63df1b1dddef4e83
Morgane <morgane@gotoandbuzz.com>
2020-05-12 09:43:58 +0200

    add images

--- src/scenes/Chp1LakeHood.js
+++ src/scenes/Chp1LakeHood.js
@@ -6,12 +6,15 @@ const Chp1LakeHood = () => {
     const history = useHistory();
 
     useEffect(() => {
-        document.getElementById('btn').addEventListener('click', () => {history.push('/simon');});
+        document.getElementById('beaver').addEventListener('click', () => {history.push('/simon');});
     }, [history])
 
     return (
         <>
-            <img src="/lakehood/lakehood.png" alt="" />
+            <img src="/lakehood/background.png" alt="" />
+            <img class="lakehood-plants" src="/lakehood/plants.png" alt="" />
+            <img id="beaver" class="lakehood-beaver" src="/lakehood/yellow-beaver.png" alt="" />
+            <img class="lakehood-beaver-x2" src="/lakehood/beaver-x2.png" alt="" />
             <button id="btn" className="skip">Skip ></button>
         </>
     )
