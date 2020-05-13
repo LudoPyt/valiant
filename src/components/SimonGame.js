e0af69787d9bf09bc6f16db3758f8e06fe37ce97
Morgane <morgane@gotoandbuzz.com>
2020-05-13 15:03:58 +0200

    interact with buttons

--- src/components/SimonGame.js
+++ src/components/SimonGame.js
@@ -19,67 +19,55 @@ class SimonGame {
         this.delta = 0;
 
         this.intersect = [];
-        this.objects = [];
+        this.btnTab = [];
         this.moves = [];
-        this.solution = ["cube0", "cube1", "cube2"];
+        this.solution = ["btn_interrupteur_haut", "btn_interrupteur_bas", "btn_pull", "btn_rotatif_haut", "btn_press"];
+
+        this.helSpeed = 0;
 
         this._setScene();
+        this._addBackground();
         this._addCockpit();
         this.mouse = new THREE.Vector2();
         this.raycaster = new THREE.Raycaster();
-        this._createCubes();
         this._setupEventListerner();
         this._render();
-    }
-
-    _createCubes() {
-        let geometry = new THREE.BoxGeometry(1, 1, 1);
-        let material1 = new THREE.MeshNormalMaterial();
-        let material2 = new THREE.MeshNormalMaterial();
-        let material3 = new THREE.MeshNormalMaterial();
-
-        let materials = [material1, material2, material3];
-
-        for(let i = 0; i < 3; i++) {
-            let cube = new THREE.Mesh(geometry, materials[i]);
-            if (i === 0) {
-                cube.position.x = -2;
-            } else if (i === 1) {
-                cube.position.x = 0;
-            } else {
-                cube.position.x = 2;
-            }
-            cube.name = "cube" + i.toString();
-            cube.material.transparent = true;
-            this.objects.push(cube);
-            this.scene.add(cube);
-        }
     }
 
     _reset() {
         console.log('reseeet')
         this.intersect = [];
         this.moves = [];
-        for (let i = 0; i < this.objects.length; i++) {
-            this.objects[i].material.opacity = 1;
-        }
+
+        this.btnTab.map(e => {
+            switch (e.name) {
+                case 'btn_interrupteur_haut':
+                    e.rotation.x = 1.7320882859168714
+                    break;
+                case 'btn_interrupteur_bas':
+                    e.rotation.x = 1.7320882859168714
+                    break;
+                case 'btn_pull':
+                    e.children[0].material.color = {r:1, g:1, b:1}
+                    break;
+                case 'btn_rotatif_haut':
+                    e.rotation.x = -1.3962007387367295
+                    break;
+                case 'btn_press':
+                    e.position.z = -1366.0050048828125
+                    break;
+                default:
+            }
+        })
     }
 
     _isRight() {
-        let isRight = [];
-
-        this.moves.map((value, key) => {
-            if (value !== this.solution[key]) {
-                return isRight.push(false);
+        for(let i = 0; i < this.solution.length; i++) {
+            if (this.solution[i] === this.moves[i]) {
+                return this.history.push('/takeoff');
             } else {
-                return isRight.push(true)
+                this._reset();
             }
-        });
-
-        if (isRight[0] === true && isRight[1] === true && isRight[2] === true) {
-            return this.history.push('/takeoff');
-        } else {
-            this._reset();
         }
     }
 
@@ -88,17 +76,41 @@ class SimonGame {
         this.mouse.y = - (event.clientY / window.innerHeight) * 2 + 1;
         this.raycaster.setFromCamera(this.mouse, this.camera);
         this.intersect = this.raycaster.intersectObjects(this.scene.children , true);
+        // console.log(this.intersect)
 
-        if (this.intersect.length > 0 && this.moves.length === 2) {
-            this.intersect[0].object.material.opacity = 0.5;
-            this.moves.push(this.intersect[0].object.name)
+        this.intersect.map(elem => {
+            return this.btnTab.map(e => {
+                if (e.name === elem.object.parent.name && !this.moves.includes(e.name)) this.moves.push(e.name)
+
+                switch (elem.object.parent.name) {
+                    case 'btn_interrupteur_haut':
+                        elem.object.parent.rotation.x = elem.object.parent.rotation.x - 1.5
+                        break;
+                    case 'btn_interrupteur_bas':
+                        elem.object.parent.rotation.x = elem.object.parent.rotation.x - 1.5
+                        break;
+                    case 'btn_pull':
+                        elem.object.parent.children[0].material.color = {r:3, g:1, b:1}
+                        this.helSpeed = 3
+                        break;
+                    case 'btn_rotatif_haut':
+                        elem.object.parent.rotation.x = elem.object.parent.rotation.x - 2
+                        this.helSpeed = 10
+                        break;
+                    case 'btn_press':
+                        elem.object.parent.position.z = elem.object.parent.position.z - 6
+                        break;
+                    default:
+                }
+
+                return this.moves
+            })
+        })
+
+        console.log(this.moves)
+
+        if (this.moves.length === 5) {
             this._isRight();
-        } else if (this.intersect.length > 0 && this.moves.length < 2) {
-            this.intersect[0].object.material.opacity = 0.5;
-            this.moves.push(this.intersect[0].object.name)
-        } else if (this.intersect.length > 0) {
-            this.moves = []
-            this.moves.push(this.intersect[0].object.name)
         }
     }
 
@@ -106,12 +118,17 @@ class SimonGame {
         document.addEventListener("click", (event) => {this._pressingDown(event)});
     }
 
+    _addBackground() {
+        let background = new THREE.TextureLoader().load('before-take-off/background.png');
+        this.scene.background = background;
+    }
+
     _setScene() {
         this.renderer = new THREE.WebGLRenderer({
             canvas: this.canvas,
             antialias: true,
             alpha: true
-            });
+        });
         this.renderer.setClearColor(0x000000);
         this.renderer.setPixelRatio(window.devicePixelRatio);
         this.renderer.setSize(window.innerWidth, window.innerHeight);
@@ -123,7 +140,7 @@ class SimonGame {
             0.1,
             1000
         );
-        this.camera.position.z = 5;
+        this.camera.position.z = -10;
 
         let light = new THREE.AmbientLight(0xffffff, .5);
         this.scene.add(light);
@@ -147,33 +164,43 @@ class SimonGame {
                         this.helices = child
                         break;
                     case 'btn_interrupteur_haut':
-                        console.log('founded')
+                        this._createTab(child)
                         break;
                     case 'btn_interrupteur_bas':
-                        console.log('founded')
+                        this._createTab(child)
                         break;
                     case 'btn_pull':
-                        console.log('founded')
+                        this._createTab(child)
                         break;
                     case 'btn_rotatif_haut':
-                        console.log('founded')
-                        break;
-                    case 'btn_rotatif_bas':
-                        console.log('founded')
+                        this._createTab(child)
                         break;
                     case 'btn_press':
-                        console.log('founded')
+                        this._createTab(child)
                         break;
+                    // case 'needle007_White_0':
+                    //     this.test = child
+                    //     break;
                     default:
-                  }
+                }
             })
+            console.log('btnTab : ', this.btnTab)
         })
+    }
+
+    _createTab(elem) {
+        elem.children[0].material.transparent = true;
+        this.btnTab.push(elem);
     }
 
     _render() {
         if (this.helices){
-            this.helices.rotation.z += 10 * this.delta
+            this.helices.rotation.z += this.helSpeed * this.delta
         }
+
+        // if (this.test) {
+        //     this.test.rotation.z += 10 * this.delta
+        // }
 
         if (!this.needDestroy) {
             requestAnimationFrame(this._render.bind(this));
--- src/scenes/Chp1Simon.js
+++ src/scenes/Chp1Simon.js
@@ -18,6 +18,7 @@ const Chp1Simon = () => {
     return (
         <>
             <canvas ref={canvas}></canvas>
+            <img src="/before-take-off/background.png" alt="" />
             <h2>SIMON GAME: find the right combinaison</h2>
         </>
     )
