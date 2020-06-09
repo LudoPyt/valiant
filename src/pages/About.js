a8d74f2cf2fef6f1844dc51a2ddc656e22274050
Morgane <morgane@gotoandbuzz.com>
2020-06-09 17:22:24 +0200

    add credits and about pages (2)

---
+++ src/pages/About.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const About = () => (
+    <main>
+        <div className="about">
+            PAGE A PROPOS
+        </div>
+    </main>
+)
+
+export default About;
---
+++ src/pages/Credits.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Credits = () => (
+    <main>
+        <div className="credits">
+            PAGE CREDITS
+        </div>
+    </main>
+)
+
+export default Credits;
