9b30f0c2aaa01b8a1a82c64d9d74f32405a0fea6
Morgane <morgane@gotoandbuzz.com>
2020-06-09 17:22:01 +0200

    add credits and about pages

--- src/pages/App.js
+++ src/pages/App.js
@@ -1,7 +1,13 @@
 import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
+import Header from '../components/Header';
+import Timeline from '../components/Timeline';
+
 import Home from './Home';
+import About from './About';
+import Credits from './Credits';
+
 import Story from './Story';
 
 import Chp1Context from '../scenes/Chp1Context';
@@ -16,9 +22,6 @@ import Chp3Landing from '../scenes/Chp3Landing';
 import Chp3Bear from '../scenes/Chp3Bear';
 import Chp3People from '../scenes/Chp3People';
 
-import Header from '../components/Header';
-import Timeline from '../components/Timeline';
-
 // import NoMatch from './404';
 // import Nuages from '../components/Nuages';
 
@@ -30,6 +33,11 @@ const App = () => (
 
     <Router>
       <Switch>
+        <Route exact path="/" component={Home} />
+        <Route exact path="/about" component={About} />
+        <Route exact path="/credits" component={Credits} />
+
+        <Route exact path="/story" component={Story} />
         <Route exact path="/context" component={Chp1Context} />
         <Route exact path="/lakehood" component={Chp1LakeHood} />
         <Route exact path="/simon" component={Chp1Simon} />
@@ -39,8 +47,7 @@ const App = () => (
         <Route exact path="/landing" component={Chp3Landing} />
         <Route exact path="/bear" component={Chp3Bear} />
         <Route exact path="/people" component={Chp3People} />
-        <Route exact path="/story" component={Story} />
-        <Route exact path="/" component={Home} />
+
         {/* <Route path="/*" component={NoMatch} /> */}
         {/* <Route path="/nuages" component={Nuages} /> */}
       </Switch>
